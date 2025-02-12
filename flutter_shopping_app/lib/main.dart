5d0d47d4ee1ff8d17c4596bf5a1cfd71fa9e8597
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2025-02-12 21:21:57 +0700

    [Remove] the debug banner

--- flutter_shopping_app/lib/main.dart
+++ flutter_shopping_app/lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shopping App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
